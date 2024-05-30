a924479707e5ce73e0be3776dabc9d6c812c15ff
bluedec <yisussaez@gmail.com>
2024-05-30 15:55:11 -0300

    Added web-socket enabled server

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# bluedec.info page
+# yesusblog.info page
 
 If this is the first time using the repo, clone with an ssh protected shell.
 
-git clone git@github.com:bluedec/bluedec.info.git
+git clone git@github.com:bluedec/yesusblog.info.git
 
 
---
+++ index-ws.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const server = require("http").createServer();
+const app = express();
+
+app.get('/', (req, res) => {
+	res.sendFile('index.html', {root: __dirname});
+});
+
+server.on('request', app);
+server.listen(3000, () => { 
+	console.log('server started on port 3000');
+});
+
+
+
+
